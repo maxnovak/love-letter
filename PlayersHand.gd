c1833509f84e9e4a7dff21ddc046e452ecbb4d6f
Maxwell Novak <novak.max@gmail.com>
2023-11-28 21:30:24 -0500

    Shift cards to being delt as they are needed.
    This is done so that there is a concept of "turns" and so that the
    player doesn't just always have 2 cards.

--- PlayersHand.gd
+++ PlayersHand.gd
@@ -8,21 +8,3 @@ func _on_Card_hover_over_card(hovering, card):
 		text.emit(Global.cardBreakdown[card._get_card()].description)
 	else:
 		card.scale = Vector2(3,3)
-
-func _on_Card_click(cardType, cardToRemove):
-	if cardType == "priest":
-		$"../OpponentHand".get_child(0)._set_visible(true)
-
-	for card in self.get_children():
-		if card != cardToRemove:
-			card.position = Vector2(-50,0)
-
-	var newCard = get_parent().deck.pop_front()
-	newCard.position = Vector2(50,0)
-	newCard._set_visible(true)
-	self.add_child(newCard)
-
-	newCard.hover_over_card.connect(self._on_Card_hover_over_card.bind(newCard))
-	newCard.clicked_card.connect(self._on_Card_click.bind(newCard))
-
-	cardToRemove.queue_free()
--- Table.gd
+++ Table.gd
@@ -1,15 +1,19 @@
 extends Node2D
 
 var deck := []
+var turn = 0
+var turnOrder := []
 
 const CardScene = preload("res://card.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	turnOrder = [$PlayersHand, $OpponentHand]
 	randomize()
 	create_deck()
 	deck.shuffle()
-	deal_cards()
+	deal_starting_cards()
+	deal_card(turnOrder[turn])
 
 func create_deck():
 	for cardType in Global.cardBreakdown:
@@ -18,16 +22,11 @@ func create_deck():
 			card.setup(Global.cardBreakdown[cardType].cardName, false)
 			deck.append(card)
 
-func deal_cards():
+func deal_starting_cards():
 	var playersCard = deck.pop_front()
 	playersCard.position = Vector2(-50,0)
 	playersCard._set_visible(true)
 	$PlayersHand.add_child(playersCard)
-
-	var playersCard2 = deck.pop_front()
-	playersCard2.position = Vector2(50,0)
-	playersCard2._set_visible(true)
-	$PlayersHand.add_child(playersCard2)
 
 	var opponentsCard = deck.pop_front()
 	$OpponentHand.add_child(opponentsCard)
@@ -35,4 +34,35 @@ func deal_cards():
 	for card in $PlayersHand.get_children():
 		card.hover_over_card.connect($PlayersHand._on_Card_hover_over_card.bind(card))
 	for card in $PlayersHand.get_children():
-		card.clicked_card.connect($PlayersHand._on_Card_click.bind(card))
+		card.clicked_card.connect(on_Card_click.bind(card))
+
+func deal_card(player):
+	var currentCard = player.get_child(0)
+	currentCard.position = Vector2(-50,0)
+
+	var newCard = deck.pop_front()
+	newCard.position = Vector2(50,0)
+	if player == $PlayersHand:
+		newCard._set_visible(true)
+	player.add_child(newCard)
+
+	if player == $PlayersHand:
+		newCard.hover_over_card.connect($PlayersHand._on_Card_hover_over_card.bind(newCard))
+		newCard.clicked_card.connect(on_Card_click.bind(newCard))
+
+func on_Card_click(cardType, cardToRemove):
+	if turnOrder[turn % turnOrder.size()] != $PlayersHand:
+		return
+	if cardType == "priest":
+		$OpponentHand.get_child(0)._set_visible(true)
+
+	for card in $PlayersHand.get_children():
+		if card != cardToRemove:
+			card.position = Vector2(0,0)
+
+	cardToRemove.queue_free()
+	next_player()
+
+func next_player():
+	turn += 1
+	deal_card(turnOrder[turn % turnOrder.size()])
