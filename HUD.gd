7e7b61407c010a285de22491957f2c198b2161ca
Maxwell Novak <novak.max@gmail.com>
2023-12-03 16:32:57 -0500

    Display card name with card description text

--- HUD.gd
+++ HUD.gd
@@ -1,12 +1,16 @@
 extends CanvasLayer
 
-func show_message(text):
-	$HelpText.text = text
+func show_message(card):
+	$HelpText.text = card.description
 	$HelpText.show()
+	$CardName.clear()
+	$CardName.push_bold()
+	$CardName.append_text(card.cardName.to_pascal_case())
+	$CardName.show()
 
 func hide_message():
 	$HelpText.hide()
+	$CardName.hide()
 
-
-func _on_players_hand_text(text):
-	show_message(text)
+func _on_players_hand_text(card):
+	show_message(card)
--- PlayersHand.gd
+++ PlayersHand.gd
@@ -5,6 +5,6 @@ signal text
 func _on_Card_hover_over_card(hovering, card):
 	if (hovering):
 		card.scale = Vector2(5,5)
-		text.emit(Global.cardBreakdown[card._get_card()].description)
+		text.emit(Global.cardBreakdown[card._get_card()])
 	else:
 		card.scale = Vector2(3,3)
